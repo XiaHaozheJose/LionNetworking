f4e174197234566077e49005783580617bebea57
BizQ <wewin.app@gmail.com>
2023-05-19 18:08:55 +0200

    add Test

---
+++ Example/Pods/Target Support Files/LionNetworking/LionNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LionNetworking : NSObject
+@end
+@implementation PodsDummy_LionNetworking
+@end
--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -6,6 +6,8 @@ class Tests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        //        networkMonitor()
+        getRequest()
     }
     
     override func tearDown() {
@@ -24,5 +26,95 @@ class Tests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    
+}
+
+//MARK: NetworkMonitor
+extension Tests {
+    func networkMonitor() {
+        let connectionType = LionNetworkMonitor.shared.connectionType
+        switch connectionType {
+        case .wifi:
+            print("当前连接类型：Wi-Fi")
+        case .cellular:
+            print("当前连接类型：蜂窝数据")
+        case .wiredEthernet:
+            print("当前连接类型：有线以太网")
+        case .unknown:
+            print("当前连接类型：未知")
+        }
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(handleNetworkStatusChanged(_:)), name: .lionNetworkStatusChanged, object: nil)
+    }
+    
+    @objc func handleNetworkStatusChanged(_ notification: Notification) {
+        if LionNetworkMonitor.shared.isConnected {
+            print("设备已连接到互联网")
+        } else {
+            print("设备未连接到互联网")
+        }
+    }
+    
+}
+
+//MARK: Request
+extension Tests {
+    
+    struct User: Codable {
+        let id: Int
+        let name: String
+        let email: String
+    }
+    struct GetLionUserRequest: LionRequestConvertible {
+        let userID: String
+        
+        func asLionRequest() -> LionNetworking.LionRequest {
+            return LionRequest(
+                //                method: .get,
+                method: .post,
+                url: "https://api.example.com/users/\(userID)",
+                headers: ["Authorization": "Bearer <your_access_token>"]
+            )
+        }
+        
+    }
+    
+    //
+    func requestTest() {
+        /**
+         let simpleRequest = LionRequest(method: .put, url: "https://api.example.com")
+         simpleRequest.headers = ["Authorization": "Bearer token"]
+         */
+        
+        let request = GetLionUserRequest(userID: "123").asLionRequest()
+        Lion.shared.execute(request: request, responseDecoder: LionJSONResponseDecoder<User>()) { result in
+            switch result {
+            case.success(let user):
+                print(user.name)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    
+    struct UploadResponse: Decodable {
+        let isSuccess: Bool
+    }
+    
+    func uploadTest() {
+        let fileURL = URL(fileURLWithPath: "app/test.pdf")
+        let fileUploader = LionFileUploader()
+        // 创建文件上传任务
+        let files = [LionUploadableFile(data: fileURL.dataRepresentation, fileName: "", mimeType: LionDefaultMimeType.pdf)]
+        
+        fileUploader.upload(request: LionRequest(url: "www.upload.com"), files: files, responseDecoder: LionJSONResponseDecoder<UploadResponse>(), progressHandler: { (progress: Double) in
+            print("progress: \(progress)")
+        }) { result in
+            // 处理上传完成的结果
+            
+        }
+    }
+    
     
 }
